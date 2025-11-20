f38732e4186b5f945d266e4287078da71ef7ad23
Saurav <ghimiresaurav789@gmail.com>
2025-11-20 16:44:25 +0545

    feat: made one small test to see working or not

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def test(request):
+    return Response({
+        'message': 'Weather Api working'
+    })
 
 # Create your views here.
