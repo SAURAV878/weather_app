5628d0c5109632c7c8d68ad9795bed3373ddb021
Saurav <ghimiresaurav789@gmail.com>
2025-11-20 22:08:23 +0545

    feat: added views.py and urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import test
+from .views import test, current_weather
 
 urlpatterns = [
     path('test/', test),
+    path('current_weather/', current_weather)
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import requests
 
 @api_view(['GET'])
 def test(request):
@@ -9,3 +10,17 @@ def test(request):
     })
 
 # Create your views here.
+@api_view(['GET'])
+def current_weather(request):
+    lat = request.GET.get('lat', '28.2096')
+    lon = request.GET.get('lon', '83.9856')
+
+    url =f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+
+    response = requests.get(url)
+
+    data =  response.json()
+
+    weather = data.get('current_weather', {})
+
+    return Response(weather)
