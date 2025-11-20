9e02f094956998270b6a2894bdef62ba034bb891
Saurav <ghimiresaurav789@gmail.com>
2025-11-20 22:58:52 +0545

    chore: some changes in views.py

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,5 @@ from .views import test, current_weather
 
 urlpatterns = [
     path('test/', test),
-    path('current_weather/', current_weather)
+    path('current_weather/', current_weather),
 ]
--- weather/views.py
+++ weather/views.py
@@ -12,15 +12,42 @@ def test(request):
 # Create your views here.
 @api_view(['GET'])
 def current_weather(request):
-    lat = request.GET.get('lat', '28.2096')
-    lon = request.GET.get('lon', '83.9856')
+    cities = {
+        "pokhara" : (28.2096, 83.9856),
+        "kathmandu" : (27.7, 85.32),
+        "biratnagar": (26.48, 87.27)
+    }
+
+    city = request.GET.get ('city', 'pokhara').lower()
+    lat, lon = cities.get(city)
 
     url =f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
 
     response = requests.get(url)
 
-    data =  response.json()
+    data = response.json()
 
     weather = data.get('current_weather', {})
+    weather['city'] = city.capitalize()
 
     return Response(weather)
+
+
+
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
